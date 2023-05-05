ffcaffafd843be52f227c267c21832785b972dab
NoorAhmed786 <aniiahmed380@gmail.com>
2023-05-05 23:53:15 +0500

    assigment-1

---
+++ app.js
@@ -0,0 +1,13 @@
+// //qustion 1
+// alert("HELLO WORLD!");
+// //qustion 2
+// alert("ERROR! please enter a correct password");
+
+// //qustion 3
+// alert("JAVASCRIPT ALERT \n \n \n Welcome to JS land  \n Happy Coding!");
+
+//qustn 5
+ alert("Hello... I can run JS through my web brower's console");
+
+//qustion 4
+ alert("Happy Coding! \nprevent this page from creating aditional dailogs.");
